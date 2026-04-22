d82f3f081e3e92afd2659e3e32d31e4553859a00
MGIC12 <mager.infante@gmail.com>
2026-04-22 12:34:03 -0300

    Configuración de react router, contiene elementos comentados porque no fueron implementados por el momento

--- tp2-react/src/App.jsx
+++ tp2-react/src/App.jsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import Error404 from "./Pages/Error404";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/favoritos" element={<Favoritos />} />
+        <Route path="/detalles/:id" element={<Detalles />} />*/}
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
   );
 }
 
