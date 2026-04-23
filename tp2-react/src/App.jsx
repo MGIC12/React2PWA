f59b35d95c025fc5b17137879652f6662f623e10
palomaStucke <paloma.stucke@est.fi.uncoma.edu.ar>
2026-04-23 10:50:49 -0300

    #4 Componente Footer, le agregue estilos con tailwind y lo implemente en la app, para ello tuve que agregar las routes a app y main

--- tp2-react/src/App.jsx
+++ tp2-react/src/App.jsx
@@ -1,10 +1,25 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import Favoritos from "./Pages/Favoritos";
 import "./App.css";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
-      <Home />
+      <Routes> 
+        <Route path="/" element={<Home />} />
+        <Route path="/favoritos" element={<Favoritos />} />
+      </Routes>
+      <Footer 
+        links={[
+          { path: "/", label: "Inicio" },
+          { path: "/favoritos", label: "Favoritos" },
+        ]}
+        social={[
+          { url: "https://facebook.com", label: "Facebook", icon: "f" },
+        ]} 
+      />
     </>
   );
 }
