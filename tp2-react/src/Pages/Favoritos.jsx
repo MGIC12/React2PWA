2c9835714ce8c49f69a95fab9216ef9c15b224ea
tebacs <matibacsay@gmail.com>
2026-04-23 18:26:41 -0300

    Modificacion en index.html para agregar un font, e implementacion del componente Header, mostrandolo en Home

--- tp2-react/src/Pages/Favoritos.jsx
+++ tp2-react/src/Pages/Favoritos.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Header from "../Components/Header/Header";
 
 export default function Favoritos() {
-  return <div>Favoritos</div>;
+  return (
+    <div>
+      <Header />
+      <div>Favoritos</div>
+    </div>
+  );
 }
